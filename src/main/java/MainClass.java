4f2c05be6dc525b960e0a46dcbf2a6a4731376fc
Norbert Kowalik <kowaliknor@gmail.com>
2020-06-26 14:08:40 +0200

    Testing

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,9 @@
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+
+    public static String getMessage() {
+        return "Hello, world!";
+    }
+}
---
+++ src/test/java/SomeTests.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SomeTests {
+    @Test
+    public void testSuccessfully() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testFailed() {
+        Assert.assertTrue(false);
+    }
+}
